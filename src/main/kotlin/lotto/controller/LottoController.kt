63f6bdbb7430bda16ea7abbc9bb4d63929ad5a8c
HamBeomJoon <hbj0802@naver.com>
2024-09-29 22:11:40 +0900

    feat: 구입금액 유효성 체크

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -12,4 +12,34 @@ class LottoController {
     fun buyLotto() {
         inputView.printInputBuyAmount()
         inputWinningAmount()
+
+    private fun inputWinningAmount() {
+        var isValidInput = false
+
+        while (!isValidInput) {
+            try {
+                winningAmount = Console.readLine()
+                winningAmountValidationCheck(winningAmount)
+                isValidInput = true
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
     }
+    private fun winningAmountValidationCheck(number: String) {
+        val isNumber = number.matches(NUMBER_REGEX.toRegex())
+        val isDivide = number.toInt() % 1000 == 0
+
+        require(isNumber) {
+            ("$ERROR_MESSAGE 숫자만 입력해주세요.")
+        }
+        require(isDivide) {
+            ("$ERROR_MESSAGE 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.")
+        }
+    }
+    companion object {
+        const val ERROR_MESSAGE = "[ERROR]"
+        const val SIX_NUMBER = 6
+        const val NUMBER_REGEX = "^[0-9]+"
+    }
+}
